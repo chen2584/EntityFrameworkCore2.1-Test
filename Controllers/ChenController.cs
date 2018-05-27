80ae51d0ce65fd41608363b35a87eef4f8bd9d70
Worameth Semapat <worameth.semapat@gmail.com>
2018-05-27 22:09:22 +0700

    initial

--- Controllers/ChenController.cs
+++ Controllers/ChenController.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
 
 namespace testAPI.Controllers
 {
@@ -7,9 +11,15 @@ namespace testAPI.Controllers
     public class ChenController : ControllerBase
     {
         [HttpGet]
-        public ActionResult Chen()
+        public ActionResult Chen(int num)
         {
-            return Ok("Values");
+            var chen = Request.Query["a"];
+            var result = chen;
+            var header = Request.Headers.TryGetValue("chen", out Microsoft.Extensions.Primitives.StringValues value);
+            
+            
+
+            return Ok(new { num = num, chen = Convert.ToString(chen), value = Convert.ToString(value), chennull = string.IsNullOrEmpty(chen), valuenull = string.IsNullOrEmpty(value) });
         }
     }
 }
