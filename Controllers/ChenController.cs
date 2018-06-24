0380e8d7eff0be65c0140539c7b34584fca1746a
Worameth Semapat <worameth.semapat@gmail.com>
2018-06-24 20:48:28 +0700

    test string value

--- Controllers/ChenController.cs
+++ Controllers/ChenController.cs
@@ -18,19 +18,29 @@ namespace testAPI.Controllers
     public class ChenController : ControllerBase
     {
         [HttpGet]
-        public ActionResult Chen(int num, int chen, [FromServices] IConfiguration Configuration, [FromServices] IHostingEnvironment env)
+        public ActionResult Chen(float num)
         {
-            return Ok(Configuration.GetConnectionString("dbChen"));
+            //Console.WriteLine($"Controller: {RouteData.Values["controller"]} Action: {RouteData.Values["action"]}");
+            //return Ok(Configuration.GetConnectionString("dbChen"));
             /*using(ChenDbContext db = new ChenDbContext())
             {
                 var chen = db.Customer.ToList();
                 return Ok(chen);
             }*/
-            /*var chen = Request.Query["a"];
+            StringValues testStringValue = new String[] { "Chen", "Chen2" };
+            var chen = Request.Query["a"];
             var result = chen;
             var header = Request.Headers.TryGetValue("chen", out Microsoft.Extensions.Primitives.StringValues value);
 
-            return Ok(new { num = num, chen = Convert.ToString(chen), value = Convert.ToString(value), chennull = string.IsNullOrEmpty(chen), valuenull = string.IsNullOrEmpty(value) });*/
+            Console.WriteLine("Value length is " + chen.Count);
+            foreach(var val in chen)
+            {
+                Console.WriteLine("Valus is: " + val);
+            }
+            Console.WriteLine("Full Value is " + chen);
+
+            return Ok(new { num = num, value = chen.ToString(), testStringValue = testStringValue.ToString() });
+            //return Ok(new { num = num, chen = chen, value = value, chenLength = value.ToArray().Length, valuenull = string.IsNullOrEmpty(value) });
         }
 
         [HttpPost]
